e091606ae614fdb8a2ff249dd7b1578671291da8
AndreySokolov <sokolov.a@samedia.ru>
2024-03-22 17:12:05 +0300

    add stucture files

---
+++ components/layout/SideBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <aside class="px-5 py-8 bg-sidebar h-full relative w-full">
+    <NuxtLink to="/" class="mb-10 block">
+      <NuxtImg  src="/logo.svg" alt="logo" width="100px" class="mx-auto"/>
+    </NuxtLink>
+    <button class="absolute top-2 right-3 transition-colors hover:text-primary">
+
+    </button>
+    <UiButton>Send</UiButton>
+  </aside>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="side-bar grid">
+    <LayoutSideBar />
+      <div>
+        <slot/>
+      </div>
+  </section>
+</template>
+
+<script lang='ts' setup>
+
+import SideBar from "~/components/layout/SideBar.vue";
+</script>
+
+<style scoped>
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 6fr;
+}
+
+.side-bar {
+  min-height: 100vh;
+}
+
+</style>
